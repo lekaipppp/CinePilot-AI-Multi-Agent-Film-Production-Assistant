cf921a3bc4d0b5ec0c1f2a276181737378b2efb6
lekaipppp <lekaipan259@gmail.com>
2026-07-30 13:00:33 +0200

    Director_agent_class_Scene

--- Agents/director_agent.py
+++ Agents/director_agent.py
@@ -1,1 +1,10 @@
 from pydantic import Basemodel, Field 
+#pydantic is a data validation library, it acts as a gatekeeper for my code.
+#Ensuring any data entered into the system matches the exact structure
+#Basemodel is the core building block of Pydanitc,
+#=========================================================================
+from typing import List, Optional 
+
+
+from google.adk.agents import Agent, LimAgent
+
